0ed98e88839cd6d0e3cff6e86a22c67e2291167c
Ramón <ramon5130@hotmail.com>
2025-01-29 14:28:24 -0300

    modificado

--- 02-strings.py
+++ 02-strings.py
@@ -2,6 +2,6 @@ texto = "Hola Mundo"
 print(texto.upper())
 print(texto.lower())
 print(texto.find("M"))
-NuevoTexto = texto.replace("Mun" , "Pe")
+NuevoTexto = texto.replace("H" , "Pe")
 print(texto , NuevoTexto)
-print("Mundo" in texto)
+print("Hola" in texto)
